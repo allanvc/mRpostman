d2b5089d7a7617ee5cc1b398baf8650ab5f456d6
Allan V. C. Quadros <avcastro.quadros@gmail.com>
2020-09-12 00:29:35 -0300

    doc typos

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage('Substantial changes without backward compatibility were implemented between versions 0.3.1 and 0.9.0.0.')
-  packageStartupMessage('Check the changelog at https://allanvc.github.io/mRpostman/news/index.html or the "Migrating code to mRpostman\'s R6 sintaxe" vignette.')
+  packageStartupMessage('Check the changelog at https://allanvc.github.io/mRpostman/news/index.html or the "Migrating old code to the new mRpostman\'s syntax" vignette.')
 }
